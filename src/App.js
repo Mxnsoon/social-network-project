1772e5f879005986edb5f05fd8296c9f661103d0
Mxnsoon <nigga.guy@yandex.ru>
2021-10-01 03:00:46 +0300

    app initialize

--- src/App.js
+++ src/App.js
@@ -7,22 +7,38 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import LoginPage from "./components/Login/Login";
+import {connect} from "react-redux";
+import {initializeApp} from "./redux/app-reducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
-const App = () => {
+class App extends React.Component {
 
-    return (
-        <div className='app-wrapper'>
-            <HeaderContainer />
-            <Navbar />
-            <div className='app-wrapper-content'>
-                <Route path='/dialogs' render={() => <DialogsContainer />}/>
-                <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
-                <Route path='/users' render={() => <UsersContainer /> } />
-                <Route path='/login' render={() => <LoginPage /> } />
+    componentDidMount() {
+        this.props.initializeApp();
+    }
 
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Route path='/login' render={() => <LoginPage/>}/>
+
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
+export default connect (mapStateToProps, {initializeApp}) (App);
