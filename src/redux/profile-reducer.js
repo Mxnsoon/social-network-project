72b63138e5d08a2bd06304cc6ca1e22e429a3bea
StepanovGallery <Nigga.guy@yandex.ru>
2022-03-16 14:11:41 +0300

    avatar upload

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,7 +3,8 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
-const DELETE_POST = 'DELETE_POST'
+const DELETE_POST = 'DELETE_POST';
+const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
 let initialState = {
     posts: [
@@ -37,6 +38,11 @@ const profileReducer = (state = initialState, action) => {
         case DELETE_POST: {
             return {...state, posts: state.posts.filter(p => p.id !== action.postId)}
         }
+
+        case SAVE_PHOTO_SUCCESS: {
+            return {...state, profile: {...state.profile, photos: action.photos }}
+        }
+
         default:
             return state;
     }
@@ -46,6 +52,7 @@ export const addPostActionCreator = (post) => ({type: ADD_POST, post})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
 export const deletePost = (postId) => ({type: DELETE_POST, postId})
+export const savePhotoSuccess = (photos) => ({type: SAVE_PHOTO_SUCCESS, photos})
 
 export const getUserProfile = (userId) => async (dispatch) => {
     let response = await profileAPI.getProfile(userId);
@@ -62,6 +69,13 @@ export const updateStatus = (status) => async (dispatch) => {
             if (response.data.resultCode === 0) {
                 dispatch(setStatus(status))
             }
+}
+
+export const savePhoto = (file) => async (dispatch) => {
+    let response = await profileAPI.savePhoto(file);
+    if (response.data.resultCode === 0) {
+        dispatch(savePhotoSuccess(response.data.data.photos))
+    }
 }
 
 export default profileReducer;
